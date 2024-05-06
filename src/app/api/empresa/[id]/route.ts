b500aadeae5e23f9e4ef730baf66996aec5fe636
ismael strey pereira <ismaelstrey@hotmail.com>
2024-05-06 17:40:47 -0300

    up

--- src/app/api/empresa/[id]/route.ts
+++ src/app/api/empresa/[id]/route.ts
@@ -6,45 +6,55 @@ export async function GET(
   request: Request,
   { params }: { params: { id: string } }
 ) {
-  try {
-    const { id } = params;
+  const { id } = params;
+  const consulta = await prisma.empresa.findUnique({ where: { cnpj: id } })
+  if (consulta) {
+    console.log(consulta)
 
-    const dataEmpresa = await consultarAPI(id);
+    return NextResponse.json(consulta);
+  } else {
+    try {
 
-    const data = await prisma.empresa.create({
-      data: {
-        abertura: dataEmpresa.abertura,
-        bairro: dataEmpresa.bairro,
-        capitalSocial: dataEmpresa.capital_social,
-        cep: dataEmpresa.cep,
-        cnpj: dataEmpresa.cnpj,
-        dataSituacao: dataEmpresa.data_situacao,
-        email: dataEmpresa.situacao,
-        extra: dataEmpresa.situacao,
-        logradouro: dataEmpresa.logradouro,
-        municipio: dataEmpresa.municipio,
-        naturezaJuridica: dataEmpresa.natureza_juridica,
-        nome: dataEmpresa.nome,
-        numero: dataEmpresa.numero,
-        porte: dataEmpresa.porte,
-        situacao: dataEmpresa.situacao,
-        status: dataEmpresa.status,
-        telefone: dataEmpresa.telefone,
-        tipo: dataEmpresa.tipo,
-        uf: dataEmpresa.uf,
-        complemento: dataEmpresa.complemento,
-        efr: dataEmpresa.efr,
-        fantasia: dataEmpresa.fantasia,
-        motivoSituacao: dataEmpresa.motivo_situacao,
-        situacaoEspecial: dataEmpresa.situacao_especial,
-        ultimaAtualizacao: dataEmpresa.ultima_atualizacao,
-        dataSituacaoEspecial: dataEmpresa.data_situacao_especial,
-      },
-    });
 
-    return NextResponse.json(data);
-  } catch (error) {
-    console.log(error);
-    return NextResponse.json(error);
+      const dataEmpresa = await consultarAPI(id);
+
+      const data = await prisma.empresa.create({
+        data: {
+          abertura: dataEmpresa.abertura,
+          bairro: dataEmpresa.bairro,
+          capitalSocial: dataEmpresa.capital_social,
+          cep: dataEmpresa.cep,
+          cnpj: dataEmpresa.cnpj,
+          dataSituacao: dataEmpresa.data_situacao,
+          email: dataEmpresa.situacao,
+          extra: dataEmpresa.situacao,
+          logradouro: dataEmpresa.logradouro,
+          municipio: dataEmpresa.municipio,
+          naturezaJuridica: dataEmpresa.natureza_juridica,
+          nome: dataEmpresa.nome,
+          numero: dataEmpresa.numero,
+          porte: dataEmpresa.porte,
+          situacao: dataEmpresa.situacao,
+          status: dataEmpresa.status,
+          telefone: dataEmpresa.telefone,
+          tipo: dataEmpresa.tipo,
+          uf: dataEmpresa.uf,
+          complemento: dataEmpresa.complemento,
+          efr: dataEmpresa.efr,
+          fantasia: dataEmpresa.fantasia,
+          motivoSituacao: dataEmpresa.motivo_situacao,
+          situacaoEspecial: dataEmpresa.situacao_especial,
+          ultimaAtualizacao: dataEmpresa.ultima_atualizacao,
+          dataSituacaoEspecial: dataEmpresa.data_situacao_especial,
+        },
+      });
+
+      return NextResponse.json(data);
+    } catch (error) {
+      console.log(error);
+      return NextResponse.json(error);
+    }
   }
+
+
 }
--- src/app/api/empresa/route.ts
+++ src/app/api/empresa/route.ts
@@ -41,3 +41,38 @@ export async function GET(request: Request) {
     return NextResponse.json(error);
   }
 }
+export async function POST(request: Request) {
+  const dataEmpresa = await request.json();
+
+  const data = await prisma.empresa.create({
+    data: {
+      abertura: dataEmpresa.abertura,
+      bairro: dataEmpresa.bairro,
+      capitalSocial: dataEmpresa.capital_social,
+      cep: dataEmpresa.cep,
+      cnpj: dataEmpresa.cnpj,
+      dataSituacao: dataEmpresa.data_situacao,
+      email: dataEmpresa.situacao,
+      extra: dataEmpresa.situacao,
+      logradouro: dataEmpresa.logradouro,
+      municipio: dataEmpresa.municipio,
+      naturezaJuridica: dataEmpresa.natureza_juridica,
+      nome: dataEmpresa.nome,
+      numero: dataEmpresa.numero,
+      porte: dataEmpresa.porte,
+      situacao: dataEmpresa.situacao,
+      status: dataEmpresa.status,
+      telefone: dataEmpresa.telefone,
+      tipo: dataEmpresa.tipo,
+      uf: dataEmpresa.uf,
+      complemento: dataEmpresa.complemento,
+      efr: dataEmpresa.efr,
+      fantasia: dataEmpresa.fantasia,
+      motivoSituacao: dataEmpresa.motivo_situacao,
+      situacaoEspecial: dataEmpresa.situacao_especial,
+      ultimaAtualizacao: dataEmpresa.ultima_atualizacao,
+      dataSituacaoEspecial: dataEmpresa.data_situacao_especial,
+    },
+  });
+  return NextResponse.json({ data });
+}
