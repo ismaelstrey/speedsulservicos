1e118f3e503f26997e42ef6114da8b3285ed0ef4
ismael strey pereira <ismaelstrey@hotmail.com>
2024-05-06 22:48:07 -0300

    update api integration witch postgree and bugfix api

--- src/app/api/empresa/[id]/route.ts
+++ src/app/api/empresa/[id]/route.ts
@@ -1,60 +1,16 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
-import { consultarAPI } from "@/services/getCnpj";
 
 export async function GET(
   request: Request,
   { params }: { params: { id: string } }
 ) {
   const { id } = params;
-  const consulta = await prisma.empresa.findUnique({ where: { cnpj: id } })
-  if (consulta) {
-    console.log(consulta)
-
+  try {
+    const consulta = await prisma.empresa.findUnique({ where: { cnpj: id } });
     return NextResponse.json(consulta);
-  } else {
-    try {
-
-
-      const dataEmpresa = await consultarAPI(id);
-
-      const data = await prisma.empresa.create({
-        data: {
-          abertura: dataEmpresa.abertura,
-          bairro: dataEmpresa.bairro,
-          capitalSocial: dataEmpresa.capital_social,
-          cep: dataEmpresa.cep,
-          cnpj: dataEmpresa.cnpj,
-          dataSituacao: dataEmpresa.data_situacao,
-          email: dataEmpresa.situacao,
-          extra: dataEmpresa.situacao,
-          logradouro: dataEmpresa.logradouro,
-          municipio: dataEmpresa.municipio,
-          naturezaJuridica: dataEmpresa.natureza_juridica,
-          nome: dataEmpresa.nome,
-          numero: dataEmpresa.numero,
-          porte: dataEmpresa.porte,
-          situacao: dataEmpresa.situacao,
-          status: dataEmpresa.status,
-          telefone: dataEmpresa.telefone,
-          tipo: dataEmpresa.tipo,
-          uf: dataEmpresa.uf,
-          complemento: dataEmpresa.complemento,
-          efr: dataEmpresa.efr,
-          fantasia: dataEmpresa.fantasia,
-          motivoSituacao: dataEmpresa.motivo_situacao,
-          situacaoEspecial: dataEmpresa.situacao_especial,
-          ultimaAtualizacao: dataEmpresa.ultima_atualizacao,
-          dataSituacaoEspecial: dataEmpresa.data_situacao_especial,
-        },
-      });
-
-      return NextResponse.json(data);
-    } catch (error) {
-      console.log(error);
-      return NextResponse.json(error);
-    }
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(error);
   }
-
-
 }
