8ab2ee468bec8b9f6b815a01baf049cee2990c34
ismael strey pereira <ismaelstrey@hotmail.com>
2024-05-21 08:46:24 -0300

    update check

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                // matching all API routes
+                source: "/api/:path*",
+                headers: [
+                    // omitted for brevity...
+                ]
+            },
+            {
+                source: "/api/special-data",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "false" },
+                    { key: "Access-Control-Allow-Origin", value: "https://ackee.speedsul.pro" },
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date" },
+                ]
+            }
+        ]
+    }
+};
 
 export default nextConfig;
