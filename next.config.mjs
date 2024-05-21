289d22b9fc95392a430830ea5190a29454b55666
ismael strey pereira <ismaelstrey@hotmail.com>
2024-05-21 08:54:03 -0300

    update check allow cors

--- next.config.mjs
+++ next.config.mjs
@@ -4,18 +4,12 @@ const nextConfig = {
         return [
             {
                 // matching all API routes
-                source: "/:path*",
+                source: "/api/:path*",
                 headers: [
-                    // omitted for brevity...
-                ]
-            },
-            {
-                source: "/special-data",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "false" },
-                    { key: "Access-Control-Allow-Origin", value: "https://ackee.speedsul.pro" },
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date" },
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" },
+                    { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
                 ]
             }
         ]
